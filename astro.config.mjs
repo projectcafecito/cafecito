e1e86dbe8b9cc51a416092b7d432ca5dabbebb7a
Soumit Salman Rahman <soumitsr@gmail.com>
2026-07-16 11:55:13 -0400

    adding tex-style formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export default defineConfig({
   site: 'https://cafecito.tech',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   redirects: {
     '/company/': '/docs/overview/',
     '/docs/company-info/': '/docs/overview/',
